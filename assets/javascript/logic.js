104e1e92c1ab1b07ee77b39516cafe2a92ddcd3f
MattsPoche <MatthewsPoche@comcast.net>
2018-09-23 18:36:08 -0500

    Added favorites

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var queryStr = "https://api.giphy.com/v1/gifs/search?q=";
     var limit = 10;
     var topics = ["dog", "cat", "snake"];
+    var favorites = [];
 
     displayButtons();
 
@@ -25,16 +26,40 @@ $(document).ready(function(){
         results.forEach(element => {
             var gif = $("<img>");
             var card = $("<div>").addClass("card").append($("<div>").addClass("card-body").append($("<h5>").addClass("card-title").text("Rating: "+element.rating)))
+            var fbutton = $("<button>").addClass("btn btn-primary addTo-favorite-button").text("Add to Favorites");
             gif.addClass( "gif-img", "img-fluid");
             gif.attr("src", element.images.fixed_height_still.url);
             gif.attr("data-isStill", "1");
             gif.attr("data-still", element.images.fixed_height_still.url);
             gif.attr("data-animate", element.images.fixed_height.url)
+            gif.attr("data-rating", element.rating);
             gif.attr("alt", "gif");
             card.append(gif);
+            card.append(fbutton);
             $("#gif-div").append(card);
         });
     }
+    function displayFavorites(){
+        $("#gif-div").empty();
+        for(var i = 0; i < favorites.length; i++){
+            var element = favorites[i];
+            var gif = $("<img>");
+            var card = $("<div>").addClass("card").append($("<div>").addClass("card-body").append($("<h5>").addClass("card-title").text("Rating: "+element.rating)))
+            var fbutton = $("<button>").addClass("btn btn-primary remove-favorite-button").text("Remove from Favorites");
+            gif.addClass( "gif-img", "img-fluid");
+            gif.attr("src", element.still);
+            gif.attr("data-isStill", "1");
+            gif.attr("data-still", element.still);
+            gif.attr("data-animate", element.animate);
+            gif.attr("data-rating", element.rating);
+            gif.attr("data-index", i);
+            gif.attr("alt", "gif");
+            card.append(gif);
+            card.append(fbutton);
+            $("#gif-div").append(card);
+        }
+    }
+
     $("#add-topic-button").on("click", function(event){
         event.preventDefault();
 
@@ -54,6 +79,11 @@ $(document).ready(function(){
         if(reg.test(num)){
             limit = num;
         }
+    });
+
+    $("#show-favorite-button").on("click", function(event){
+        event.preventDefault();
+        displayFavorites();
     });
 
     $(document).on("click", ".topic-button", function(event){
@@ -83,6 +113,26 @@ $(document).ready(function(){
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-isStill", "1");
         }
+    });
+
+    $(document).on("click", ".addTo-favorite-button", function(event){
+        event.preventDefault();
+
+        var gif = $(this).parent().find("img");
+        favorites.push({
+            "animate": $(gif).attr("data-animate"),
+            "still": $(gif).attr("data-still"),
+            "rating": $(gif).attr("data-rating")
+        });
+    });
+    $(document).on("click", ".remove-favorite-button", function(event){
+        event.preventDefault();
+
+        var gif = $(this).parent().find("img");
+        favorites.splice(gif.attr("data-index"), 1);
+        displayFavorites();
+        console.log(favorites);
+        
     });
 
 
